90be9ddd56c36702b09e781eceecdb78d943b0d6
Maryury Ramos <Maryuryramos05@gmail.com>
2025-03-19 17:51:02 -0300

    vista password

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import LoginScreen from './App/login';
-
+import ChangePasswordScreen from './App/password';
+//<LoginScreen />
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <LoginScreen />
+      
+      <ChangePasswordScreen />
     </SafeAreaView>
   );
 };
